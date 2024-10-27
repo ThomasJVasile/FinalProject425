231aba63fae8ff05d275954971236777ac894c4d
adamdavis1211 <adamndavis12@gmail.com>
2024-10-26 19:18:25 -0700

    some message with what u are changeing / adding

--- src/router.js
+++ src/router.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "./components/HomePage.vue";
 import LoginPage from "./components/LoginPage.vue";
 import CreateEvent from "./components/CreateEvent.vue";  
+import RegisterPage from "./components/RegisterPage.vue";
 
 const routes = [
   { path: "/homepage", name: "HomePage", component: HomePage },
   { path: "/log-in", name: "LoginPage", component: LoginPage },
   { path: "/create-event", name: "CreateEvent", component: CreateEvent },  // Add CreateEvent route
+  { path: "/RegisterPage", name: "RegisterPage", component: RegisterPage},
   { path: "/", redirect: "/homepage" }  
 ];
 
