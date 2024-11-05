0bcacb18fdffb8567b78ab44934319458f375636
adamdavis1211 <adamndavis12@gmail.com>
2024-11-04 19:29:39 -0800

    Register user and add them to database

--- src/components/RegisterPage.vue
+++ src/components/RegisterPage.vue
@@ -2,30 +2,54 @@
     <div>
         <h1> Register now </h1>
     </div>
-    <form>
+    <form @submit.prevent="register">
         <label for="email">Email:</label>
-        <input type="text" id="email" name="email"/>
+        <input v-model="email" type="text" id="email" name="email"/>
         <br>
         <label for="username">Username:</label>
-        <input type="text" id="username" name="username"/>
+        <input v-model="username" type="text" id="username" name="username"/>
         <br>
         <label for="password">Password:</label>
-        <input type="text" id="password" name="password"/>
+        <input v-model="password" type="text" id="password" name="password"/>
         <br>
         <label for="confirmpassword">Confirm Password:</label>
-        <input type="text" id="confirmpassword" name="confirmpassword"/>
+        <input v-modle= "confirmpassword" type="text" id="confirmpassword" name="confirmpassword"/>
         <br>
         <button type="submit">Register Now</button>
     </form>
 </template> 
 
-<!--
-TESTING COMMIT
--->
-
 <script>
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { ref } from "vue";
+import { UseRouter } from "vue-router";
+
 export default {
-  name: 'RegisterPage',
+  setup() {
+    const router = UseRouter();
+    const email = ref("");
+    const username = ref("");
+    const password = ref(""); 
+    const confirmPassword = ref(""); 
+
+    const register = async () => {
+      if (password.value != confirmPassword.value) {
+        alert("Passwords do not match.");
+        return;
+      }
+      try {
+        const auth = getAuth();
+        await createUserWithEmailAndPassword(auth, email.value, password.value);
+        alert("Registration successful.");
+        router.push("/homepage");
+      }
+      catch (error) {
+        alert("Registration failed: " + error.message);
+      }
+
+    };
+    return {email, username, password, confirmPassword, register};
+  }
 };
 </script>
 
