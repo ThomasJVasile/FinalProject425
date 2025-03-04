17233550ab9ce2de9ee85a147fe57ddbde500282
Thomas-John <vasilethomasjohn@gmail.com>
2025-03-03 19:02:41 -0800

    changed the package.json

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,19 @@
+// module.exports = {
+//   preset: '@vue/cli-plugin-unit-jest',
+//   testEnvironment: 'jsdom',
+//   transform: {
+//     '^.+\\.vue$': 'vue-jest',
+//     '^.+\\.js$': 'babel-jest'
+//   },
+//   moduleFileExtensions: ['js', 'json', 'vue'],
+//   transformIgnorePatterns: ['/node_modules/']
+// };
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest',
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.vue$': 'vue-jest',
-    '^.+\\.js$': 'babel-jest'
-  },
-  moduleFileExtensions: ['js', 'json', 'vue'],
-  transformIgnorePatterns: ['/node_modules/']
-};
+    testEnvironment: "jsdom",
+    moduleFileExtensions: ["js", "json", "vue"],
+    transform: {
+      "^.+\\.vue$": "@vue/vue3-jest",
+      "^.+\\.js$": "babel-jest"
+    }
+  };
+  
