5a8fecee0b737b7d90b186e21c2aa35ef058a940
Thomas-John <123978285+ThomasJVasile@users.noreply.github.com>
2025-04-16 19:32:10 -0700

    server

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
+// // vue.config.js
+// module.exports = {
+//   devServer: {
+//     host: '0.0.0.0',
+//     port: 9090,
+//   },
+// };
+
+
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true,
+    devServer: {
+        allowedHosts: "all"
+    }
 })
