af9ecd3e58806d285ab4fb0faa615bd459696ad7
Thomas-John <123978285+ThomasJVasile@users.noreply.github.com>
2025-05-01 21:15:31 -0700

    new register page that looks better

--- src/components/RegisterPage.vue
+++ src/components/RegisterPage.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <v-container class="fill-height d-flex justify-center align-center">
     <v-card class="pa-5" elevation="10">
       <v-card-title class="text-center">
@@ -35,7 +35,54 @@
       </v-card-text>
     </v-card>
   </v-container>
+</template> -->
+
+<template>
+  <v-container class="fill-height d-flex justify-center align-center">
+    <v-card class="register-card elevation-12">
+      <v-card-title class="text-h5 text-center font-weight-bold">
+        <v-icon class="mr-2">mdi-account-plus</v-icon>
+        Register Now
+      </v-card-title>
+      <v-card-text>
+        <v-form @submit.prevent="register">
+          <v-row dense>
+            <v-col cols="12" sm="6">
+              <v-text-field v-model="firstName" label="First Name" prepend-inner-icon="mdi-account" dense outlined required />
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-text-field v-model="lastName" label="Last Name" prepend-inner-icon="mdi-account" dense outlined required />
+            </v-col>
+            <v-col cols="12">
+              <v-text-field v-model="email" label="Email" type="email" prepend-inner-icon="mdi-email" dense outlined required />
+            </v-col>
+            <v-col cols="12">
+              <v-text-field v-model="username" label="Username" prepend-inner-icon="mdi-account-circle" dense outlined required />
+            </v-col>
+            <v-col cols="12">
+              <v-text-field v-model="password" label="Password" type="password" prepend-inner-icon="mdi-lock" dense outlined required />
+            </v-col>
+            <v-col cols="12">
+              <v-text-field v-model="confirmpassword" label="Confirm Password" type="password" prepend-inner-icon="mdi-lock-check" dense outlined required />
+            </v-col>
+          </v-row>
+
+          <v-alert v-if="errorMessage" type="error" class="mt-3" dense text>
+            {{ errorMessage }}
+          </v-alert>
+          <v-alert v-if="successMessage" type="success" class="mt-3" dense text>
+            {{ successMessage }}
+          </v-alert>
+
+          <v-btn type="submit" color="primary" block class="mt-4">
+            Register Now
+          </v-btn>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
+
 
 <script>
 import { getAuth, createUserWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
@@ -133,4 +180,13 @@ export default {
   max-width: 650px !important;
   width: 100%;
 }
+
+.register-card {
+  width: 100%;
+  max-width: 500px;
+  padding: 24px;
+  border-radius: 16px;
+  box-shadow: 0 4px 24px rgba(0, 0, 0, 0.1);
+}
+
 </style>
